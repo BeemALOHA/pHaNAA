ccb4359cb3e51a64a58f43553d4bbcb99a854b28
BeemALOHA <beemkub007@gmail.com>
2026-03-07 13:18:51 +0700

    sda

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,83 +3,99 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
+    [Header("Movement")]
     public float speed;
     public float jumpForce;
     private float inputHorizontal;
     private float inputVertical;
     private Rigidbody2D rb;
     private bool facingRight = true;
-
-    [Header("jump")]
-    public int extraJumpValue;
+    [Header("Jump")]
+    public int extraJumpsValue;
     public float jumpTime;
-    private int extraJump;
+    private int extraJumps;
     private float jumpTimeCounter;
     private bool isJumping;
-
-    [Header("Ground Check")]
+    [Header("Ground")]
+    private bool isGrounded;
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
-    private bool isGrounded;
-    [Header("Effects")]
+    [Header("Ladder")]
+    public float distance;
+    public LayerMask whatIsLadder;
+    private bool isClimbing;
+    RaycastHit2D hitInfo;
+    [Header("Effect")]
+    public GameObject dustEffect;
+    private bool spawnDust;
     public Animator camAnimator;
-    private bool canPlayCamShake;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator playerAnimator;
+
     void Start()
     {
-       rb = GetComponent<Rigidbody2D>();
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
     }
     void FixedUpdate()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius,whatIsGround);
         inputHorizontal = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(inputHorizontal * speed, rb.linearVelocity.y);
-
-        if (!facingRight && inputHorizontal > 0)
+        if(facingRight == false && inputHorizontal > 0)
+        {
             Flip();
-        else if (facingRight && inputHorizontal < 0)
+        }
+        else if(facingRight == true && inputHorizontal < 0)
+        {
             Flip();
+        }
     }
-
     void Update()
     {
+        PlayerInput();
         if (isGrounded == true)
         {
-            if (canPlayCamShake)
-            { 
-                canPlayCamShake = false;
-                camAnimator.SetTrigger("shake");              
+            if(spawnDust == true)
+            {
+                camAnimator.SetTrigger("shake");
+                Instantiate(dustEffect, groundCheck.position, Quaternion.identity);
+                spawnDust = false;
             }
         }
         else
-        
         {
-            canPlayCamShake = true;
+            spawnDust = true;
         }
 
-
-        if (isGrounded)
+        if (isGrounded == true)
         {
-            extraJump = extraJumpValue;
-
-            if (rb.linearVelocity.y < 0)
+            extraJumps = extraJumpsValue;
+            if (rb.linearVelocity.y == 0)
+            {
                 isJumping = false;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && extraJump > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
         {
+            Instantiate(dustEffect, groundCheck.position, Quaternion.identity);
             isJumping = true;
             jumpTimeCounter = jumpTime;
             rb.linearVelocity = Vector2.up * jumpForce;
-            extraJump--;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded == true)
+        {
+            Instantiate(dustEffect, groundCheck.position, Quaternion.identity);
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.linearVelocity = Vector2.up * jumpForce;
         }
 
         if (Input.GetKey(KeyCode.Space) && isJumping)
         {
-            if (jumpTimeCounter > 0)
+            if(jumpTimeCounter > 0)
             {
                 rb.linearVelocity = Vector2.up * jumpForce;
                 jumpTimeCounter -= Time.deltaTime;
@@ -89,23 +105,49 @@ public class PlayerController : MonoBehaviour
                 isJumping = false;
             }
         }
-
-        if (Input.GetKeyUp(KeyCode.Space))
+        if(Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;
         }
+
+        hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+        if (hitInfo.collider != null)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            isClimbing = false;
+        }
+
+        if (isClimbing == true && hitInfo.collider != null)
+        {
+            inputVertical = Input.GetAxisRaw("Vertical");
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, inputVertical * speed);
+            rb.gravityScale = 0;
+        }
+        else
+        {
+            rb.gravityScale = 5;
+        }
     }
-    private void Flip()
+
+    void Flip()
     {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
     }
-    private void OnDrawGizmos()
+    void PlayerInput()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, checkRadius);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            playerAnimator.SetTrigger("victory");
+        }
     }
 }
 
---
+++ Assets/Script/Destoryer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Destoryer : MonoBehaviour
+{
+    public float timeToDestroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
